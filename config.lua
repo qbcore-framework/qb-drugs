2dd935fb0ff04241aacb68369751b76bf67b7483
vanillaa <80897991+vanillla1@users.noreply.github.com>
2021-08-19 14:41:16 +0300

    update vecto3 and fix duplicate coords& add mdealer
    update vector3 and fix duplicate coords and add multi dealer options

--- config.lua
+++ config.lua
@@ -24,100 +24,236 @@ Config.RandomInt = function(length)
 	end
 end
 
-Config.Products = {
-    [1] = {
-        name = "weed_white-widow",
-        price = 15,
-        amount = 150,
-        info = {},
-        type = "item",
-        slot = 1,
-        minrep = 0,
+Config.Dealers = {
+			[1] = {
+				["name"] = "Chapo",
+				["coords"] = vector3(764.44, -3207.92, 6.03),
+				["time"] = {
+					["min"] = 9,
+					["max"] = 15,
+				},
+				["products"] = {
+			[1] = {
+				name = "weed_white-widow",
+				price = 15,
+				amount = 150,
+				info = {},
+				type = "item",
+				slot = 1,
+				minrep = 0,
+			},
+			[2] = {
+				name = "weed_skunk",
+				price = 15,
+				amount = 150,
+				info = {},
+				type = "item",
+				slot = 2,
+				minrep = 20,
+			},
+			[3] = {
+				name = "weed_purple-haze",
+				price = 15,
+				amount = 150,
+				info = {},
+				type = "item",
+				slot = 3,
+				minrep = 40,
+			},
+			[4] = {
+				name = "weed_og-kush",
+				price = 15,
+				amount = 150,
+				info = {},
+				type = "item",
+				slot = 4,
+				minrep = 60,
+			},
+			[5] = {
+				name = "weed_amnesia",
+				price = 15,
+				amount = 150,
+				info = {},
+				type = "item",
+				slot = 5,
+				minrep = 80,
+			},
+			[6] = {
+				name = "weed_white-widow_seed",
+				price = 15,
+				amount = 150,
+				info = {},
+				type = "item",
+				slot = 6,
+				minrep = 100,
+			},
+			[7] = {
+				name = "weed_skunk_seed",
+				price = 15,
+				amount = 150,
+				info = {},
+				type = "item",
+				slot = 7,
+				minrep = 120,
+			},
+			[8] = {
+				name = "weed_purple-haze_seed",
+				price = 15,
+				amount = 150,
+				info = {},
+				type = "item",
+				slot = 8,
+				minrep = 140,
+			},
+			[9] = {
+				name = "weed_og-kush_seed",
+				price = 15,
+				amount = 150,
+				info = {},
+				type = "item",
+				slot = 9,
+				minrep = 160,
+			},
+			[10] = {
+				name = "weed_amnesia_seed",
+				price = 15,
+				amount = 150,
+				info = {},
+				type = "item",
+				slot = 10,
+				minrep = 180,
+			},
+        },
     },
     [2] = {
-        name = "weed_skunk",
-        price = 15,
-        amount = 150,
-        info = {},
-        type = "item",
-        slot = 2,
-        minrep = 20,
+        ["name"] = "Escobar",
+		["coords"] = vector3(-1108.57, -1643.44, 4.64),
+        ["time"] = {
+            ["min"] = 20,
+            ["max"] = 03,
+        },
+        ["products"] = {
+			[1] = {
+				["name"] = "Chapo",
+				["coords"] = vector3(764.44, -3207.92, 6.03),
+				["time"] = {
+					["min"] = 9,
+					["max"] = 15,
+				},
+				["products"] = {
+			[1] = {
+				name = "weed_white-widow",
+				price = 15,
+				amount = 150,
+				info = {},
+				type = "item",
+				slot = 1,
+				minrep = 0,
+			},
+			[2] = {
+				name = "weed_skunk",
+				price = 15,
+				amount = 150,
+				info = {},
+				type = "item",
+				slot = 2,
+				minrep = 20,
+			},
+			[3] = {
+				name = "weed_purple-haze",
+				price = 15,
+				amount = 150,
+				info = {},
+				type = "item",
+				slot = 3,
+				minrep = 40,
+			},
+			[4] = {
+				name = "weed_og-kush",
+				price = 15,
+				amount = 150,
+				info = {},
+				type = "item",
+				slot = 4,
+				minrep = 60,
+			},
+			[5] = {
+				name = "weed_amnesia",
+				price = 15,
+				amount = 150,
+				info = {},
+				type = "item",
+				slot = 5,
+				minrep = 80,
+			},
+			[6] = {
+				name = "weed_white-widow_seed",
+				price = 15,
+				amount = 150,
+				info = {},
+				type = "item",
+				slot = 6,
+				minrep = 100,
+			},
+			[7] = {
+				name = "weed_skunk_seed",
+				price = 15,
+				amount = 150,
+				info = {},
+				type = "item",
+				slot = 7,
+				minrep = 120,
+			},
+			[8] = {
+				name = "weed_purple-haze_seed",
+				price = 15,
+				amount = 150,
+				info = {},
+				type = "item",
+				slot = 8,
+				minrep = 140,
+			},
+			[9] = {
+				name = "weed_og-kush_seed",
+				price = 15,
+				amount = 150,
+				info = {},
+				type = "item",
+				slot = 9,
+				minrep = 160,
+			},
+			[10] = {
+				name = "weed_amnesia_seed",
+				price = 15,
+				amount = 150,
+				info = {},
+				type = "item",
+				slot = 10,
+				minrep = 180,
+			},
+
+        },
     },
     [3] = {
-        name = "weed_purple-haze",
-        price = 15,
-        amount = 150,
-        info = {},
-        type = "item",
-        slot = 3,
-        minrep = 40,
-    },
-    [4] = {
-        name = "weed_og-kush",
-        price = 15,
-        amount = 150,
-        info = {},
-        type = "item",
-        slot = 4,
-        minrep = 60,
-    },
-    [5] = {
-        name = "weed_amnesia",
-        price = 15,
-        amount = 150,
-        info = {},
-        type = "item",
-        slot = 5,
-        minrep = 80,
-    },
-    [6] = {
-        name = "weed_white-widow_seed",
-        price = 15,
-        amount = 150,
-        info = {},
-        type = "item",
-        slot = 6,
-        minrep = 100,
-    },
-    [7] = {
-        name = "weed_skunk_seed",
-        price = 15,
-        amount = 150,
-        info = {},
-        type = "item",
-        slot = 7,
-        minrep = 120,
-    },
-    [8] = {
-        name = "weed_purple-haze_seed",
-        price = 15,
-        amount = 150,
-        info = {},
-        type = "item",
-        slot = 8,
-        minrep = 140,
-    },
-    [9] = {
-        name = "weed_og-kush_seed",
-        price = 15,
-        amount = 150,
-        info = {},
-        type = "item",
-        slot = 9,
-        minrep = 160,
-    },
-    [10] = {
-        name = "weed_amnesia_seed",
-        price = 15,
-        amount = 150,
-        info = {},
-        type = "item",
-        slot = 10,
-        minrep = 180,
+        ["name"] = "RickRoss",
+		["coords"] = vector3(-50.25, 1911.26, 195.71),
+        ["time"] = {
+            ["min"] = 1,
+            ["max"] = 23,
+        },
+        ["products"] = {
+            [1] = {
+                name = "bandage",
+                price = 100,
+                amount = 50,
+                info = {},
+                type = "item",
+                slot = 1,
+                minrep = 0,
+            }
+        },
     },
 }
-
-Config.Dealers = {}
 
 Config.CornerSellingDrugsList = {
     "weed_white-widow",
@@ -173,53 +309,375 @@ Config.DrugsPrice = {
 Config.DeliveryLocations = {
     [1] = {
         ["label"] = "Stripclub",
-        ["coords"] = {
-            ["x"] = 106.24,
-            ["y"] = -1280.32,
-            ["z"] = 29.24,
-        }
+        ["coords"] = vector3(106.24, -1280.32, 29.24),
     },
     [2] = {
         ["label"] = "Vinewood Video",
-        ["coords"] = {
-            ["x"] = 223.98,
-            ["y"] = 121.53,
-            ["z"] = 102.76,
-        }
+		["coords"] = vector3(223.98, 121.53, 102.76),
     },
     [3] = {
-        ["label"] = "Vinewood Video",
-        ["coords"] = {
-            ["x"] = 223.98,
-            ["y"] = 121.53,
-            ["z"] = 102.76,
-        }
+        ["label"] = "Taxi",
+		["coords"] = vector3(882.67, -160.26, 77.11),
     },
     [4] = {
         ["label"] = "Resort",
-        ["coords"] = {
-            ["x"] = -1245.63,
-            ["y"] = 376.21,
-            ["z"] = 75.34,
-        }
+		["coords"] = vector3(-1245.63, 376.21, 75.34),
     },
     [5] = {
         ["label"] = "Bahama Mamas",
-        ["coords"] = {
-            ["x"] = -1383.1,
-            ["y"] = -639.99,
-            ["z"] = 28.67,
-        }
+		["coords"] = vector3(-1383.1, -639.99, 28.67),
     },
 }
 
 Config.CornerSellingZones = {
     [1] = {
-        ["coords"] = {
-            ["x"] = -1415.53,
-            ["y"] = -1041.51,
-            ["z"] = 4.62,
+		["coords"] = vector3(-1415.53, -1041.51, 4.62),
+        ["time"] = {
+            ["min"] = 12,
+            ["max"] = 18,
         },
+    },
+}
+
+Config.DeliveryItems = {
+    [1] = {
+        ["item"] = "weed_brick",
+        ["minrep"] = 0,
+    },
+}Config = Config or {}
+Config.MinimumDrugSalePolice = 2
+
+local StringCharset = {}
+local NumberCharset = {}
+
+for i = 48,  57 do table.insert(NumberCharset, string.char(i)) end
+for i = 65,  90 do table.insert(StringCharset, string.char(i)) end
+for i = 97, 122 do table.insert(StringCharset, string.char(i)) end
+
+Config.RandomStr = function(length)
+	if length > 0 then
+		return Config.RandomStr(length-1) .. StringCharset[math.random(1, #StringCharset)]
+	else
+		return ''
+	end
+end
+
+Config.RandomInt = function(length)
+	if length > 0 then
+		return Config.RandomInt(length-1) .. NumberCharset[math.random(1, #NumberCharset)]
+	else
+		return ''
+	end
+end
+
+Config.Dealers = {
+			[1] = {
+				["name"] = "Chapo",
+				["coords"] = vector3(764.44, -3207.92, 6.03),
+				["time"] = {
+					["min"] = 9,
+					["max"] = 15,
+				},
+				["products"] = {
+			[1] = {
+				name = "weed_white-widow",
+				price = 15,
+				amount = 150,
+				info = {},
+				type = "item",
+				slot = 1,
+				minrep = 0,
+			},
+			[2] = {
+				name = "weed_skunk",
+				price = 15,
+				amount = 150,
+				info = {},
+				type = "item",
+				slot = 2,
+				minrep = 20,
+			},
+			[3] = {
+				name = "weed_purple-haze",
+				price = 15,
+				amount = 150,
+				info = {},
+				type = "item",
+				slot = 3,
+				minrep = 40,
+			},
+			[4] = {
+				name = "weed_og-kush",
+				price = 15,
+				amount = 150,
+				info = {},
+				type = "item",
+				slot = 4,
+				minrep = 60,
+			},
+			[5] = {
+				name = "weed_amnesia",
+				price = 15,
+				amount = 150,
+				info = {},
+				type = "item",
+				slot = 5,
+				minrep = 80,
+			},
+			[6] = {
+				name = "weed_white-widow_seed",
+				price = 15,
+				amount = 150,
+				info = {},
+				type = "item",
+				slot = 6,
+				minrep = 100,
+			},
+			[7] = {
+				name = "weed_skunk_seed",
+				price = 15,
+				amount = 150,
+				info = {},
+				type = "item",
+				slot = 7,
+				minrep = 120,
+			},
+			[8] = {
+				name = "weed_purple-haze_seed",
+				price = 15,
+				amount = 150,
+				info = {},
+				type = "item",
+				slot = 8,
+				minrep = 140,
+			},
+			[9] = {
+				name = "weed_og-kush_seed",
+				price = 15,
+				amount = 150,
+				info = {},
+				type = "item",
+				slot = 9,
+				minrep = 160,
+			},
+			[10] = {
+				name = "weed_amnesia_seed",
+				price = 15,
+				amount = 150,
+				info = {},
+				type = "item",
+				slot = 10,
+				minrep = 180,
+			},
+        },
+    },
+    [2] = {
+        ["name"] = "Escobar",
+		["coords"] = vector3(-1108.57, -1643.44, 4.64),
+        ["time"] = {
+            ["min"] = 20,
+            ["max"] = 03,
+        },
+        ["products"] = {
+			[1] = {
+				["name"] = "Chapo",
+				["coords"] = vector3(764.44, -3207.92, 6.03),
+				["time"] = {
+					["min"] = 9,
+					["max"] = 15,
+				},
+				["products"] = {
+			[1] = {
+				name = "weed_white-widow",
+				price = 15,
+				amount = 150,
+				info = {},
+				type = "item",
+				slot = 1,
+				minrep = 0,
+			},
+			[2] = {
+				name = "weed_skunk",
+				price = 15,
+				amount = 150,
+				info = {},
+				type = "item",
+				slot = 2,
+				minrep = 20,
+			},
+			[3] = {
+				name = "weed_purple-haze",
+				price = 15,
+				amount = 150,
+				info = {},
+				type = "item",
+				slot = 3,
+				minrep = 40,
+			},
+			[4] = {
+				name = "weed_og-kush",
+				price = 15,
+				amount = 150,
+				info = {},
+				type = "item",
+				slot = 4,
+				minrep = 60,
+			},
+			[5] = {
+				name = "weed_amnesia",
+				price = 15,
+				amount = 150,
+				info = {},
+				type = "item",
+				slot = 5,
+				minrep = 80,
+			},
+			[6] = {
+				name = "weed_white-widow_seed",
+				price = 15,
+				amount = 150,
+				info = {},
+				type = "item",
+				slot = 6,
+				minrep = 100,
+			},
+			[7] = {
+				name = "weed_skunk_seed",
+				price = 15,
+				amount = 150,
+				info = {},
+				type = "item",
+				slot = 7,
+				minrep = 120,
+			},
+			[8] = {
+				name = "weed_purple-haze_seed",
+				price = 15,
+				amount = 150,
+				info = {},
+				type = "item",
+				slot = 8,
+				minrep = 140,
+			},
+			[9] = {
+				name = "weed_og-kush_seed",
+				price = 15,
+				amount = 150,
+				info = {},
+				type = "item",
+				slot = 9,
+				minrep = 160,
+			},
+			[10] = {
+				name = "weed_amnesia_seed",
+				price = 15,
+				amount = 150,
+				info = {},
+				type = "item",
+				slot = 10,
+				minrep = 180,
+			},
+
+        },
+    },
+    [3] = {
+        ["name"] = "RickRoss",
+		["coords"] = vector3(-50.25, 1911.26, 195.71),
+        ["time"] = {
+            ["min"] = 1,
+            ["max"] = 23,
+        },
+        ["products"] = {
+            [1] = {
+                name = "bandage",
+                price = 100,
+                amount = 50,
+                info = {},
+                type = "item",
+                slot = 1,
+                minrep = 0,
+            }
+        },
+    },
+}
+
+Config.CornerSellingDrugsList = {
+    "weed_white-widow",
+    "weed_skunk",
+    "weed_purple-haze",
+    "weed_og-kush",
+    "weed_amnesia",
+    "weed_ak47",
+    "crack_baggy",
+    "cokebaggy",
+    "meth"
+}
+
+Config.DrugsPrice = {
+    ["weed_white-widow"] = {
+        min = 15,
+        max = 24,
+    },
+    ["weed_og-kush"] = {
+        min = 15,
+        max = 28,
+    },
+    ["weed_skunk"] = {
+        min = 15,
+        max = 31,
+    },
+    ["weed_amnesia"] = {
+        min = 18,
+        max = 34,
+    },
+    ["weed_purple-haze"] = {
+        min = 18,
+        max = 37,
+    },
+    ["weed_ak47"] = {
+        min = 18,
+        max = 40,
+    },
+    ["crack_baggy"] = {
+        min = 18,
+        max = 34,
+    },
+    ["cokebaggy"] = {
+        min = 18,
+        max = 37,
+    },
+    ["meth"] = {
+        min = 18,
+        max = 40,
+    },
+}
+
+Config.DeliveryLocations = {
+    [1] = {
+        ["label"] = "Stripclub",
+        ["coords"] = vector3(106.24, -1280.32, 29.24),
+    },
+    [2] = {
+        ["label"] = "Vinewood Video",
+		["coords"] = vector3(223.98, 121.53, 102.76),
+    },
+    [3] = {
+        ["label"] = "Taxi",
+		["coords"] = vector3(882.67, -160.26, 77.11),
+    },
+    [4] = {
+        ["label"] = "Resort",
+		["coords"] = vector3(-1245.63, 376.21, 75.34),
+    },
+    [5] = {
+        ["label"] = "Bahama Mamas",
+		["coords"] = vector3(-1383.1, -639.99, 28.67),
+    },
+}
+
+Config.CornerSellingZones = {
+    [1] = {
+		["coords"] = vector3(-1415.53, -1041.51, 4.62),
         ["time"] = {
             ["min"] = 12,
             ["max"] = 18,

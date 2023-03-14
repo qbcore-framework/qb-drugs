b25e63dd8c3fbf5b3100d9114a36e991e8609d64
Charlie <dev@charlie.fyi>
2023-03-14 23:58:32 +0000

    Fix error on server load when no configured dealers

--- client/deliveries.lua
+++ client/deliveries.lua
@@ -240,6 +240,8 @@ function AwaitingInput()
 end
 
 function InitZones()
+    if #Config.Dealers == 0 then return end
+
     if Config.UseTarget then
         for k,v in pairs(Config.Dealers) do
             exports["qb-target"]:AddBoxZone("dealer_"..k, vector3(v.coords.x, v.coords.y, v.coords.z), 1.5, 1.5, {

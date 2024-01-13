462c11d3a0dcc49c0c2670f4187819050c383058
Kayne <155355908+kaynegraham@users.noreply.github.com>
2024-01-13 13:55:09 +1100

    add: example of adding a dealer in config.lua

--- config.lua
+++ config.lua
@@ -2,7 +2,34 @@ Config = {
     Debug = true,                                          -- true / false - Currently prints the vector3 and label of locations when requesting a delivery
     NearbyDeliveries = true,                               -- true / false - Do you want deliveries to be within a certain amount of units?
     DeliveryWithin = 2000,                                 -- int (Default 2000) - How many units do you want the delivery location to be within from the player when making a delivery request?
-    Dealers = {},
+    Dealers = {
+        -- Example:
+    --     ['Sandy Dealer'] = {
+    --         time = {min = 5, max = 23},
+    --         name = "LiL Shady",
+    --         coords = {x = 1894.4, y = 3895.88, z = 33.19},
+    --         products = { 
+    --             [1] = {
+    --                 name = "weed_white-widow",
+    --                 price = 15,
+    --                 amount = 150,
+    --                 info = {},
+    --                 type = "item",
+    --                 slot = 1,
+    --                 minrep = 0,
+    --             },
+    --             [2] = {
+    --                 name = "weed_skunk",
+    --                 price = 15,
+    --                 amount = 150,
+    --                 info = {},
+    --                 type = "item",
+    --                 slot = 2,
+    --                 minrep = 0,
+    --             },
+    --         },
+       -- },
+    },
     UseTarget = GetConvar('UseTarget', 'false') == 'true', -- Use qb-target interactions (don't change this, go to your server.cfg and add setr UseTarget true)
     PoliceCallChance = 99,                                 --in percentage (if 99, theres the 99% to call the police)
 
